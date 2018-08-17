d7e908ea2dec0a9a1c730764b24212d2713b06a2
Guy <guy@ubeya.com>
2018-08-17 19:10:22 +0300

    firebase dev npm

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,4 +1,6 @@
-import firebase from "firebase";
+import firebase from "firebase/app";
+import "firebase/auth";
+import  "firebase/database";
 
 const config = {
     apiKey: "AIzaSyD99j8D124StULlW9rS-8xxzNT5D_L1KhQ",
